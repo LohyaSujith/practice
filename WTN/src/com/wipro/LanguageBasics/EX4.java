34064fbc710e8159c6bca4b54053d1b1bcd46192
Dell <Dell@DESKTOP-P68EVEM>
2020-06-12 08:27:30 +0530

    Fourth Program Of PBL in Basics

---
+++ WTN/src/com/wipro/LanguageBasics/EX4.java
@@ -0,0 +1,12 @@
+package com.wipro.LanguageBasics;
+
+public class EX4 {
+	 public static void main(String ar[])
+     {
+           int x,y,s;
+           x=Integer.parseInt(ar[0]);
+           y=Integer.parseInt(ar[1]);
+           s=x+y;
+           System.out.println("sum of " + x + " and " + y +" is " +s);
+     }
+}
