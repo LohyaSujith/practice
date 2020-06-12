eb8934625e1dc47e524cef8ac64d04d210e28ad6
Dell <Dell@DESKTOP-P68EVEM>
2020-06-12 08:31:00 +0530

    First Program in LOOPS in PBL

---
+++ WTN/src/com/wipro/Loops/L2.java
@@ -0,0 +1,14 @@
+package com.wipro.Loops;
+public class L2 {
+	public static void main(String[] args)
+	{
+		  int x;
+          x=Integer.parseInt(args[0]);
+          if(x%2==0)
+          {
+        	  System.out.println("Even");
+          }
+          else
+        	  System.out.println("Odd");
+}
+}
