7a7e2d202a9d75c3f6838648556d5f4a9fb6c700
Dell <Dell@DESKTOP-P68EVEM>
2020-06-12 08:33:26 +0530

    Fifth Program in LOOPS iN PBL

---
+++ WTN/src/com/wipro/Loops/L5.java
@@ -0,0 +1,19 @@
+package com.wipro.Loops;
+
+public class L5 {
+	public static void main(String [] args)
+	 {
+		 char i1='0';
+		 if(i1>=97 && i1<=122)
+		 {
+			 System.out.println("Character");
+		 }
+		 else if(i1>0)
+		 {
+			 System.out.println("Digit");
+		 }
+		 else
+			 System.out.println("SPECIAL CHARACTER!");
+
+}
+}
