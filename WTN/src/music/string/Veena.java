8538e4fe43c78b3ea0ac8e9e11ece2dd87690a2e
Dell <Dell@DESKTOP-P68EVEM>
2020-06-24 19:03:28 +0530

    Interface Example Package String

---
+++ WTN/src/music/string/Veena.java
@@ -0,0 +1,13 @@
+/*Write a class called Veena which implements Playable interface. 
+Let this class be placed in a package music.string */
+
+
+
+package music.string;
+import music.Playable;
+public class Veena implements Playable{
+ public void play()
+ {
+	 System.out.println("Veena plays");
+ }
+}
