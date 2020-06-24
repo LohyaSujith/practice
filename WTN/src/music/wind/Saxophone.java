aaab514ba8bb4b71962ca33efe84af81acd1eb04
Dell <Dell@DESKTOP-P68EVEM>
2020-06-24 19:04:19 +0530

    Interface Example Package Wind

---
+++ WTN/src/music/wind/Saxophone.java
@@ -0,0 +1,12 @@
+/*Write a class called Saxophone which implements Playable interface. 
+Let this class be placed in a package music.wind */
+
+package music.wind;
+import music.Playable;
+public class Saxophone implements Playable{
+	 public void play()
+	 {
+		 System.out.println("Saxophone plays");
+	 }
+
+}
