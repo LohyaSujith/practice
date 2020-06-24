dd07aa6d93ac9fe1aaadb3946b5d3af93582f7ef
Dell <Dell@DESKTOP-P68EVEM>
2020-06-24 19:02:06 +0530

    Interface Example Package Music

---
+++ WTN/src/music/Playable.java
@@ -0,0 +1,8 @@
+/* Write an interface called Playable, with a method
+void play();
+Let this interface be placed in a package called music. */
+
+package music;
+public interface  Playable {
+	void play();
+}
