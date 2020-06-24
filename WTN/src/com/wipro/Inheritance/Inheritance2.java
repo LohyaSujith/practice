9d26e25526489b2e6f2887bfeccdc856324dcc67
Dell <Dell@DESKTOP-P68EVEM>
2020-06-24 08:45:54 +0530

    Second Problem in Inheritance

---
+++ WTN/src/com/wipro/Inheritance/Inheritance2.java
@@ -0,0 +1,10 @@
+package com.wipro.Inheritance;
+public class Inheritance2{
+	public static void main(String[] args)
+	{
+		Person p= new Person("sujith");
+		Employee e=new Employee("avinash",10000,2017,"abcde");
+		System.out.println(p);
+		System.out.println(e);
+	}
+}
