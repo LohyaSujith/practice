36e4c5cfcad5f028d5cea7060fb0b4ae45f0b045
Dell <Dell@DESKTOP-P68EVEM>
2020-07-06 13:14:02 +0530

    Wrapper Classes

---
+++ WTN/src/wrapperClasses/WC3.java
@@ -0,0 +1,16 @@
+package wrapperClasses;
+import java.util.*;
+public class WC3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		String output = String.format("%8s", Integer.toBinaryString(input)).replace(' ', '0');
+		System.out.println(output);
+		
+		
+		sc.close();
+
+	}
+}
